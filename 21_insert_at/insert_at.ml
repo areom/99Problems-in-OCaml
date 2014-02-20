41a823f50a5897a2a1106d7627e51c19eeb749ad
Weiyuan Li <liwy34@gmail.com>
2014-02-20 16:58:11 -0500

    Minor update.

--- 21_insert_at/insert_at.ml
+++ 21_insert_at/insert_at.ml
@@ -1,5 +1,6 @@
 (* nsert an element at a given position into a list. *)
 let insert_at it i l =
+    (* do it tail recursively *)
     let rec aux i first = function
         | [] -> List.rev (it::first)
         | hd::tl as snd -> if i = 0 then (List.rev first) @ (it :: snd)
