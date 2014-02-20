0afc358658eae4e26f79dd794776c984f926c907
Weiyuan Li <liwy34@gmail.com>
2014-02-20 16:15:33 -0500

    21_insert_at

---
+++ 21_insert_at/insert_at.ml
@@ -0,0 +1,8 @@
+(* nsert an element at a given position into a list. *)
+let insert_at it i l =
+    let rec aux i first = function
+        | [] -> List.rev (it::first)
+        | hd::tl as snd -> if i = 0 then (List.rev first) @ (it :: snd)
+                                    else aux (i - 1) (hd::first) tl
+    in
+    aux i [] l
