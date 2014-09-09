58bcada9c5fa36cbeaa92691aef25e1856a98055
Weiyuan Li <liwy34@gmail.com>
2014-09-08 20:28:18 -0400

    33 prime factors.

---
+++ 33_prime_factors/33_factors.ml
@@ -0,0 +1,7 @@
+(* Determine the prime factors of a given positive integer. *)
+let factors n = 
+    let rec aux d n =
+        if n = 1 then [] else
+            if (n mod d) = 0 then d :: (aux d (n/d)) else aux (d+1) n
+    in
+    aux 2 n
